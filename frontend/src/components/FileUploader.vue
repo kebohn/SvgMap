665d38d58be052b4fa81c147ec900586b2b74397
kestre <kevin.streiter@ius.uzh.ch>
2020-06-23 15:50:26 +0200

    insert link nodes as buttons

--- frontend/src/components/FileUploader.vue
+++ frontend/src/components/FileUploader.vue
@@ -1,36 +1,5 @@
 <template>
   <section>
-    <b-field label="ProjectName">
-      <b-input v-model="projectName"></b-input>
-    </b-field>
-    <b-field>
-      <b-upload name="files" @input="checkFiles()" v-model="dropFile" drag-drop expanded>
-        <section class="section">
-          <div class="content has-text-centered">
-            <p>
-              <b-icon icon="upload" size="is-large"> </b-icon>
-            </p>
-            <p>Drop your SVG-File here or click to upload</p>
-          </div>
-        </section>
-      </b-upload>
-    </b-field>
-
-    <div class="tags" v-if="dropFile">
-      <span
-        class="tag is-primary"
-      >
-        {{ dropFile.name }}
-        <button
-          class="delete is-small"
-          type="button"
-          @click="deleteDropFile()"
-        ></button>
-      </span>
-    </div>
-    <svg-container :file=file v-on:updateLinkList="updateLinkList"></svg-container>
-
-
     <div class="field is-grouped m-t-md is-pulled-right">
       <div class="control">
         <button @click="submit()" class="button is-link">Submit</button>
@@ -40,6 +9,48 @@
       </div>
     </div>
     <div class="is-clearfix"></div>
+
+    <b-field label="ProjectName">
+      <b-input v-model="projectName"></b-input>
+    </b-field>
+    <b-field>
+      <div class="buttons">
+          <b-button size="is-small"
+                 v-for="(link, index) in links"
+                 :key="index" v-bind:class="clicked === index ? 'is-warning' : 'is-danger'" @click="activateBtn(index)">
+            {{link.getAttribute("xlink:title")}}
+          </b-button>
+      </div>
+    </b-field>
+
+    <b-field>
+      <b-upload name="files" @input="checkFiles()" v-model="dropFiles" multiple drag-drop expanded>
+        <section class="section">
+          <div class="content has-text-centered">
+            <p>
+              <b-icon icon="upload" size="is-large"> </b-icon>
+            </p>
+            <p>Drop your File here or click to upload</p>
+          </div>
+        </section>
+      </b-upload>
+    </b-field>
+
+    <div class="tags">
+      <span
+        v-for="(file, index) in dropFiles"
+        :key="index"
+        class="tag is-primary"
+      >
+        {{ file.name }}
+        <button
+          class="delete is-small"
+          type="button"
+          @click="deleteDropFile(index)"
+        ></button>
+      </span>
+    </div>
+    <svg-container :file=file v-on:updateLinkList="updateLinkList"></svg-container>
   </section>
 </template>
 
@@ -50,16 +61,18 @@ export default {
   components: {SvgContainer},
   data() {
     return {
-      dropFile: null,
+      dropFiles: [],
       file: null,
-      projectName: ''
+      projectName: '',
+      links: [],
+      clicked:''
     };
   },
   methods: {
     submit() {
         let formData = new FormData();
-        for(let i = 0; i < this.dropFile.length; i++ ) {
-          let file = this.dropFile[i];
+        for(let i = 0; i < this.dropFiles.length; i++ ) {
+          let file = this.dropFiles[i];
           formData.append('files[' + i + ']', file);
         }
         this.$http.post('/api/projects', formData, {
@@ -69,17 +82,32 @@ export default {
         }
         ).then(response => console.log(response));
     },
-    deleteDropFile() {
-      this.dropFile = null;
-      this.file = '';
+    deleteDropFile(index) {
+      if (this.getFileExtension(this.dropFiles[index].name) === 'svg') {
+        this.dropFiles = [];
+        this.file = null;
+        this.links = [];
+      } else {
+        this.dropFiles.splice(index, 1);
+      }
     },
     checkFiles() {
-      const reader = new FileReader();
-      reader.readAsText(this.dropFile);
-      reader.onload = e => this.file = e.target.result;
+      for (let file of this.dropFiles) {
+        if (this.getFileExtension(file.name) === 'svg') {
+          const reader = new FileReader();
+          reader.readAsText(file);
+          reader.onload = e => this.file = e.target.result;
+        }
+      }
     },
     updateLinkList(list) {
-      console.log(list);
+      this.links = list;
+    },
+    activateBtn(e){
+      this.clicked = e;
+    },
+    getFileExtension(file) {
+      return file.split('.').pop().toLowerCase()
     }
   },
 };
