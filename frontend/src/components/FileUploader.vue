30680209f16b16683ed0e6568b2ecae9e3b40d2a
kestre <kevin.streiter@ius.uzh.ch>
2020-06-24 09:03:18 +0200

    color toggle according to file upload

--- frontend/src/components/FileUploader.vue
+++ frontend/src/components/FileUploader.vue
@@ -2,7 +2,10 @@
   <section>
     <div class="field is-grouped m-t-md is-pulled-right">
       <div class="control">
-        <button @click="submit()" class="button is-link">Submit</button>
+        <button
+                v-bind:disabled="uploaded.length === this.dropFiles - 1 && projectName !== '' ?
+                isDisabled = false : isDisabled = true"
+                @click="submit()" class="button is-link">Submit</button>
       </div>
       <div class="control">
         <button class="button is-link is-light">Back</button>
@@ -17,20 +20,22 @@
       <div class="buttons">
           <b-button size="is-small"
                  v-for="(link, index) in links"
-                 :key="index" v-bind:class="clicked === index ? 'is-warning' : 'is-danger'" @click="activateBtn(index)">
+                 :key="index"
+                  v-bind:class="uploaded[index] !== -1 ? 'is-success' : 'is-danger'"
+                  @click="activateBtn(index)">
             {{link.getAttribute("xlink:title")}}
           </b-button>
       </div>
     </b-field>
 
-    <b-field>
-      <b-upload name="files" @input="checkFiles()" v-model="dropFiles" multiple drag-drop expanded>
+    <b-field v-if="showFileUpload">
+      <b-upload name="files" @input.once="checkFirstFile()" @input="addFileToLink()" v-model="dropFiles" multiple drag-drop expanded>
         <section class="section">
           <div class="content has-text-centered">
             <p>
               <b-icon icon="upload" size="is-large"> </b-icon>
             </p>
-            <p>Drop your File here or click to upload</p>
+            <p>Drop your files here or click to upload, the first uploaded file <b>must</b> be a <q>.svg</q> file</p>
           </div>
         </section>
       </b-upload>
@@ -40,7 +45,7 @@
       <span
         v-for="(file, index) in dropFiles"
         :key="index"
-        class="tag is-primary"
+        class="tag is-info"
       >
         {{ file.name }}
         <button
@@ -65,7 +70,10 @@ export default {
       file: null,
       projectName: '',
       links: [],
-      clicked:''
+      clicked: Number,
+      uploaded: [],
+      showFileUpload: true,
+      isDisabled: true
     };
   },
   methods: {
@@ -84,30 +92,67 @@ export default {
     },
     deleteDropFile(index) {
       if (this.getFileExtension(this.dropFiles[index].name) === 'svg') {
-        this.dropFiles = [];
-        this.file = null;
-        this.links = [];
+        this.resetFileUploadForm();
       } else {
+        console.log(index)
         this.dropFiles.splice(index, 1);
+        let idx = this.uploaded.indexOf(index);
+        this.uploaded[idx] = -1;
       }
     },
-    checkFiles() {
-      for (let file of this.dropFiles) {
-        if (this.getFileExtension(file.name) === 'svg') {
+    checkFirstFile() {
+        if (this.getFileExtension(this.dropFiles[0].name) === 'svg') {
           const reader = new FileReader();
-          reader.readAsText(file);
+          reader.readAsText(this.dropFiles[0]);
           reader.onload = e => this.file = e.target.result;
-        }
+        } else {
+          this.dropFiles = [];
+          this.$buefy.toast.open({
+            message:`Please upload a ".svg"-file`,
+            type: 'is-danger'
+          });
+          this.resetFileUploadForm();
+
+      }
+    },
+    addFileToLink() {
+      for (let [indexEntry, item] of this.links.entries()) {
+        for (let [indexFile, file] of this.dropFiles.entries())
+          if (file.name === this.getBaseName(item.getAttribute("xlink:href"))) {
+            this.uploaded[indexEntry] = indexFile;
+          }
       }
     },
     updateLinkList(list) {
-      this.links = list;
+      for (let item of list) {
+        if (!this.isUrl(item.getAttribute("xlink:href"))) {
+          this.links.push(item);
+        }
+      }
+      this.uploaded = new Array(this.links.length).fill(-1);
     },
-    activateBtn(e){
+    activateBtn(e) {
       this.clicked = e;
+    },
+     getBaseName(str) {
+       return str.substr(str.lastIndexOf("\\") + 1);
     },
     getFileExtension(file) {
       return file.split('.').pop().toLowerCase()
+    },
+    isUrl(file) {
+        let regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@\-/]))?/
+        return regexp.test(file.toLowerCase());
+    },
+    resetFileUploadForm() {
+      this.showFileUpload = false;
+      this.$nextTick(()=> {
+        this.dropFiles = [];
+        this.file = null;
+        this.clicked = null;
+        this.links = [];
+        this.showFileUpload = true;
+      })
     }
   },
 };
